5707e4608429428c89d7e6318dd2f09fe266d1da
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-06 21:31:22 -0500

    install the app test

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,20 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = async ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+    include: path.resolve(__dirname, "../"),
+  });
+
+  config.plugins.push(new MiniCssExtractPlugin({ filename: "[name].css" }));
+
   return {
     ...config,
     resolve: {
       ...config.resolve,
-      modules: [path.resolve(__dirname, "../src"), ...config.resolve.modules]
-    }
+      modules: [path.resolve(__dirname, "../src"), ...config.resolve.modules],
+    },
   };
 };
