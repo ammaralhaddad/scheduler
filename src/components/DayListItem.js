015ad693ec33b2229ea9743037461a3e5e0dc741
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-07 15:19:49 -0500

    add the classNmae Styling

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,24 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+  const formatSpots = (spots) => {
+    return `${spots || "no"} spot${spots === 1 ? "" : "s"}`;
+  };
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li
+      className={dayClass.replace(/ /g, "")}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
+      <h2>{props.name}</h2>
+      <h3>{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
