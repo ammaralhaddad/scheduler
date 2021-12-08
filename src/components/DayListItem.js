c04929133426dc02cbe6aa7b4bb1c4d96e10b7aa
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-07 19:09:32 -0500

    fix the if statment

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,22 +3,27 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  let dayClass = classNames("day-list__item", {
-    "--selected": props.selected,
-    "--full": !props.spots,
-  });
-  const formatSpots = (spots) => {
-    return `${spots || "no"} spot${spots === 1 ? "" : "s"}`;
-  };
+  let formatSpots = function (spots) {
+    if (spots === 0) {
+      return " Sorry no spots remaining";
+    }
 
+    if (spots === 1) {
+      return "1 spot remaining";
+    } else return `${spots} Yay spots remaining`;
+  };
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
   return (
     <li
-      className={dayClass.replace(/ /g, "")}
-      onClick={() => props.setDay(props.name)}
       data-testid="day"
+      onClick={() => props.setDay(props.name)}
+      className={dayClass}
     >
-      <h2>{props.name}</h2>
-      <h3>{formatSpots(props.spots)} remaining</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
