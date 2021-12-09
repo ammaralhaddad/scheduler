d572070887f5728b69b876dc7a85c2d31f82cc9d
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-09 12:36:59 -0500

    fix errorr is show.js so the name and the interviewer name will show up on the appoitment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -9,7 +9,10 @@ export default function Appointment(props) {
     <>
       <Header time={props.time} />
       {props.interview ? (
-        <Show student={props.student} interviewer={props.interviewer} />
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
       ) : (
         <Empty />
       )}
