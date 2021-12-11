f880b6483721e19a7e409668a33096fe492a326e
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-10 19:19:52 -0500

    import use visaul mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,10 @@ import "components/Appointment/style.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
   return (
