346292676cc6948e50bec3276256d48a0d54ef2a
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-11 01:11:47 -0500

    add the second test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,21 +1,13 @@
 import { useState } from "react";
+
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode, replace = false) => {
-    if (!replace) {
-      setHistory(() => [...history, newMode]);
-    }
-    setMode(() => newMode);
+  const transition = function (newMode) {
+    return setMode(newMode);
   };
 
-  const back = () => {
-    if (history[history.length - 1] !== initial) {
-      history.pop();
-      setMode(() => history[history.length - 1]);
-    }
-  };
+  const back = function () {};
 
-  return { mode, transition, back };
+  return { mode, transition };
 }
